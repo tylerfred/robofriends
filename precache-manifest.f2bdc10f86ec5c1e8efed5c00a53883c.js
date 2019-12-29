0609e22eacc35c83d5c01e1e73df7a868a8bc095
Tyler Fredrickson <tylerdavidfred@gmail.com>
2019-12-29 07:53:17 -0600

    Updates

--- precache-manifest.f2bdc10f86ec5c1e8efed5c00a53883c.js
+++ precache-manifest.f2bdc10f86ec5c1e8efed5c00a53883c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "72ebd2787dea8214e4e5356e672535a5",
+    "revision": "313700d7de1de14a22c4038d310f0489",
     "url": "/robofriends/index.html"
   },
   {
@@ -8,16 +8,16 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
   },
   {
-    "revision": "45b37256e912c9f3b577",
-    "url": "/robofriends/static/css/main.0ff520dc.chunk.css"
+    "revision": "2c05820f7a617b59a5a6",
+    "url": "/robofriends/static/css/main.feedfdb7.chunk.css"
   },
   {
     "revision": "db030a0313823e763c87",
     "url": "/robofriends/static/js/2.fbad7c07.chunk.js"
   },
   {
-    "revision": "45b37256e912c9f3b577",
-    "url": "/robofriends/static/js/main.0593408a.chunk.js"
+    "revision": "2c05820f7a617b59a5a6",
+    "url": "/robofriends/static/js/main.7fa805e2.chunk.js"
   },
   {
     "revision": "15646e7ae3f7e7d4e926",
